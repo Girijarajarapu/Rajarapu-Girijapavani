80eb81c15211e9a3d59fc235ad3040d71fe6b75b
Girijarajarapu <girijapavani09@gmail.com>
2025-07-01 13:58:26 +0530

    Add files via upload

---
+++ python assignment/python assignment/p4.py
@@ -0,0 +1,6 @@
+interior=float(input("enter the interior cost of painting:"))
+interior_area=float(input("enter the interior_area cost of painting:"))
+exterior=float(input("enter the exterior cost of painting:"))
+exterior_area=float(input("enter the exterior cost of painting:"))
+total_cost=interior*interior_area+exterior*exterior_area
+print("the total cost is:",total_cost)
