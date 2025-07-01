861913fbaddc60e9985448326bed79942d3173ea
Girijarajarapu <girijapavani09@gmail.com>
2025-07-01 13:39:52 +0530

    Add files via upload

---
+++ python assignment/p3.py
@@ -0,0 +1,5 @@
+distance=float(input("enter the distance  (km):"))
+meters=distance*1000
+centimeters=meters*100
+print("the distance in metersis:",meters)
+print("the distance in centimeters:",centimeters)
