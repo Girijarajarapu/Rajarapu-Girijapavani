aa9d167b4a205c6d5c52e61d30170bf3ca857f1e
Girijarajarapu <girijapavani09@gmail.com>
2025-07-02 09:29:20 +0530

    Add files via upload

---
+++ python assignment/p1.py
@@ -0,0 +1,5 @@
+p=float(input("enter the principal amount(p):"))
+t=float(input("enter the time (T):"))
+r=float(input("enter the rate of interest(R):"))
+SI=p*t*r/100
+print("the simple interest is",SI)
